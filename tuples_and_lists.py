992d624d3d2373131356f34ecd0b1a3e9c4f360b
lunAr-creator <lunArcreator3@gmail.com>
2021-04-10 16:34:01 +0100

    updated uples_and_lists.py

--- tuples_and_lists.py
+++ tuples_and_lists.py
@@ -37,3 +37,68 @@ for word in words:
 # Modify a tuple?? YOU CANT!!! THEY ARE IMMUTABLE!!!!
 
 #=================================LISTS=======================================#
+
+'''
+Lists are good for keeping track of things by their order, espeially when the order and contents
+might change. Unlike string, lists are mutable. You can change a list in place, add new elements 
+and delete or replace existing elements. The same value can occur more than once in a list. 
+'''
+
+# Create lists with [] (A list's contents are sperated by commas and surrounded by square brackets)
+
+example_list = []
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursdar', 'Saturday', 'Sunday']
+leap_years = [2000, 2004, 2008]
+
+# Create or convert with list() (you can make a list with the list function)
+
+another_empty_list = list()
+
+# The list function can convert other iterable data types into a list aswell (eg a tuple)
+
+a_tuple = ('ready', 'fire', 'aim')
+list(a_tuple)
+
+# CREATE A LIST FROM A STRING WITH SPLIT
+
+day = '1/1/2021'
+print(day.split('/'))
+
+# GET AN ITEM FROM A LIST BY OFFSET
+
+# As with strings, you can extract a single value from a list by specifying its offset.
+
+nums = ['1', '2', '3']
+for i in range(len(nums)):
+	print(nums[i])
+
+print(nums[1]) # will return 2
+
+# GET ITEMS WITH SLICE
+
+# You can extract a subsequence of a list by using slice (a slice of a list is also a list)
+
+nums = ['1', '2', '3', '4']
+print(nums[0:3])
+
+# As with strings, slices can step by values other than one. The next example starts and the beginning and goes right by 2:
+
+print(nums[::2])
+
+# Here we go left by 2
+
+print(nums[::-2])
+
+# and finally, a trick to REVERSE A LIST
+
+print(nums[::-1])
+
+# Although, none of these slices changed the nums list itself, because we didnt assign them to nums. To reverse a list in place, we can use list.reverse()
+
+nums.reverse()
+
+# ADD AN ITEM TO THE END OF A LIST WITH APPEND
+
+print(nums)
+nums.append(5)
+print(nums)
